7b2244cfc3f2f8e6d03e30e1e173b71fa67db936
Sergei Druzhina <sdruzhin@deakin.edu.au>
2020-10-04 17:10:22 +1100

    Created a new task POST route

--- api/requester-api.js
+++ api/requester-api.js
@@ -18,4 +18,36 @@ router.get('/requester/tasks', (req, res) => {
     });
 });
 
+// Create a new task
+router.post('/requester/tasks', (req, res) => {
+
+    // TODO: change 
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+
+    const task = new Task({ 
+        requesterId: '5f765a458f54040004e70a61', // TODO - change to current user
+        title: req.body.title,
+        description: req.body.description,
+        type: req.body.type,
+        responses: req.body.responses,
+        master: req.body.master,
+        numberWorkers: req.body.numberWorkers,
+        reward: req.body.reward,
+        expiry: expiry,         // TODO 
+        createdAt: Date.now()   // TODO 
+    });
+    task.save((err) => {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json({ 
+                status: 'success', 
+                message: 'Task successfully added.' 
+            })
+        }
+    });
+});
+
 module.exports = router;
